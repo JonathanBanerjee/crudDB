a4de15ce629f84888ae0095c368e601969c279b5
banochocolat <jonathan@youd.net>
2022-01-27 13:54:04 +0000

    AWSDB and gitignore

---
+++ app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+var express = require('express');
+var app = express();
+var  https = require('https');
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
+// error handler
+app.use(function(err, req) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
